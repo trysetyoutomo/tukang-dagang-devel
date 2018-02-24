0c6d87aba3a08f9d73bc04c62dfe7bb71324cd97
Wildan Egi Ardiawan <wildanardiawan@gmail.com>
2018-02-24 14:41:01 +0700

    show mode realtime

--- platforms/android/assets/www/lib/framework7/js/helper.js
+++ platforms/android/assets/www/lib/framework7/js/helper.js
@@ -53,4 +53,13 @@ function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+function blink(selector){
+    $(selector).fadeOut('slow', function(){
+        $(this).fadeIn('slow', function(){
+            blink(this);
+        });
+    });
+}
+    
   
